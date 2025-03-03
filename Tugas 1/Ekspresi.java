6594ffd59ba21ecc14c6e37b0568f50cac0392dd
13020230262 - Ni'matun Nayiroh <13020230262@student.umi.ac.id>
2025-03-03 08:28:13 +0800

    Sub Folder_Tugas PBO

---
+++ Tugas 1/Ekspresi.java
@@ -0,0 +1,14 @@
+public class Ekspresi {
+
+public static void main(String[] args) {
+ 
+// TODO Auto-generated method stub
+/* KAMUS */
+ int x = 1;
+ int y = 2;
+
+ System.out.print("x = "+ x + "\n");
+ System.out.print("y = "+ y + "\n");
+ System.out.print("hasil ekspresi = (x<y)?x:y = "+ ((x < y) ? x : y));
+}
+}
---
+++ Tugas 1/Incr.java
@@ -0,0 +1,14 @@
+public class Incr {
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+int i, j;
+
+i = 3;
+j = i++;
+
+System.out.println ("Nilai i : " + (++i) +
+"\nNilai j : " + j);
+}
+}
